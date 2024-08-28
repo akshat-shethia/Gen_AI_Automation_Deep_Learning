aa1a5ada091dc691c8b9de700b723ffca6ec6613
akshat-shethia <akshatshethia@gmail.com>
2024-08-28 22:29:34 +0530

    Completed Upto Video 16 in the Machine Learning Deployment With Streamlit Course

--- Machine_Learning_Model_Deployment_with_Streamlit/15_caching_basics/starter.py
+++ Machine_Learning_Model_Deployment_with_Streamlit/15_caching_basics/starter.py
@@ -9,7 +9,31 @@ st.button('Test cache')
 
 st.subheader("st.cache_data")
 
-
 st.subheader("st.cache_resource")
 
+st.cache_data
 
+
+def cache_():
+    time.sleep(2)
+    return "I'm Done Running"
+
+
+st.write(cache_())
+
+
+@st.cache_resource
+def LR():
+    time.sleep(2)
+    X = np.array([1, 2, 3, 4, 5, 6, 7]).reshape(-1, 1)
+    Y = np.array([1, 2, 3, 4, 5, 6, 7])
+
+    model = LinearRegression().fit(X, Y)
+    return model
+
+
+lr = LR()
+X_pred = np.array([8]).reshape(-1, 1)
+Y_pred = lr.predict(X_pred)
+
+st.write(f"The Prediction is : {Y_pred[0]}")
