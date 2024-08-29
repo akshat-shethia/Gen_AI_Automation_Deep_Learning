caf608d68f3286f49b29852f832bb15dee1c4615
akshat-shethia <akshatshethia@gmail.com>
2024-08-29 19:08:10 +0530

    Completed Upto Video 32 in the Machine Learning Deployment With Streamlit Course

--- Machine_Learning_Model_Deployment_with_Streamlit/29_multipage_refactor/starter.py
+++ Machine_Learning_Model_Deployment_with_Streamlit/29_multipage_refactor/starter.py
@@ -7,7 +7,9 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-if "product" not in st.session_state:
+if all(key not in st.session_state.keys() for key in ('product', 'x1', 'x2')):
+    st.session_state['x1'] = 0
+    st.session_state['x2'] = 0
     st.session_state['product'] = 0
 
 
@@ -16,9 +18,15 @@ def multiply(x1, x2):
 
 # Workaround from: https://stackoverflow.com/questions/74968179/session-state-is-reset-in-streamlit-multipage-app
 
-# Function to keep a value (copy from temporary to permanent)
 
-# Function to reassign value to temporary key (copy from permanent to temporary)
+def keep(key):
+    # Copy from temporary widget key to permanent key
+    st.session_state[key] = st.session_state[f"_{key}"]
+
+
+def unkeep(key):
+    # Copy from permanent key to temporary widget key
+    st.session_state[f"_{key}"] = st.session_state[key]
 
 
 if __name__ == "__main__":
@@ -28,9 +36,13 @@ if __name__ == "__main__":
     col1, col2 = st.columns(2)
 
     with col1:
-        x1 = st.number_input("Pick a number", 0, 10, key="x1")
+        unkeep('x1')
+        x1 = st.number_input("Pick a number", 0, 10,
+                             key="_x1", on_change=keep, args=(("x1",)))
     with col2:
-        x2 = st.number_input("Pick another number", 0, 10, key='x2')
+        unkeep('x2')
+        x2 = st.number_input("Pick another number", 0, 10,
+                             key='_x2', on_change=keep, args=(("x2",)))
 
     st.button("Multiply!", type="primary", on_click=multiply, args=((x1, x2)))
 
