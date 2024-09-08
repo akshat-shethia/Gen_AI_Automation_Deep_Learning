eaa31e6d2f3702a888206bbd78a71e9fca849dc6
akshat-shethia <akshatshethia@gmail.com>
2024-09-08 21:27:09 +0530

    Completed till video 63 in the Langchain Course

--- LangChain_Mastery_Develop_LLM_Apps_with_LangChain_&_Pinecone/Project_Streamlit_Front_End_for_QA_App/RAG_With_Streamlit.py
+++ LangChain_Mastery_Develop_LLM_Apps_with_LangChain_&_Pinecone/Project_Streamlit_Front_End_for_QA_App/RAG_With_Streamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_openai import OpenAIEmbedding
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 import os
 
@@ -31,14 +31,16 @@ def load_document(file):
 # splitting data in chunks
 def chunk_data(data, chunk_size=256, chunk_overlap=20):
     from langchain.text_splitter import RecursiveCharacterTextSplitter
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     chunks = text_splitter.split_documents(data)
     return chunks
 
 
 # create embeddings using OpenAIEmbeddings() and save them in a Chroma vector store
 def create_embeddings(chunks):
-    embeddings = OpenAIEmbeddings(model='text-embedding-3-small', dimensions=1536)  # 512 works as well
+    embeddings = OpenAIEmbeddings(
+        model='text-embedding-3-small', dimensions=1536)  # 512 works as well
     vector_store = Chroma.from_documents(chunks, embeddings)
 
     # if you want to use a specific directory for chromadb
@@ -51,8 +53,10 @@ def ask_and_get_answer(vector_store, q, k=3):
     from langchain_openai import ChatOpenAI
 
     llm = ChatOpenAI(model='gpt-4o-mini', temperature=1)
-    retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k': k})
-    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+    retriever = vector_store.as_retriever(
+        search_type='similarity', search_kwargs={'k': k})
+    chain = RetrievalQA.from_chain_type(
+        llm=llm, chain_type="stuff", retriever=retriever)
 
     answer = chain.invoke(q)
     return answer['result']
@@ -69,3 +73,75 @@ def calculate_embedding_cost(texts):
     return total_tokens, total_tokens / 1000 * 0.00002
 
 
+if __name__ == "__main__":
+
+    import os
+    from dotenv import load_dotenv, find_dotenv
+    load_dotenv(find_dotenv(), override=True)
+
+    st.image("C:\miniconda\Gen_AI_Automation_Deep_Learning\LangChain_Mastery_Develop_LLM_Apps_with_LangChain_&_Pinecone\Project_Streamlit_Front_End_for_QA_App\img.png")
+    st.subheader("LLM Question-Answer Application 🤖")
+
+    with st.sidebar:
+        api_key = st.text_input("Enetr Your OpenAI API Key: ", type='password')
+        if api_key:
+            os.environ["OPENAI_API_KEY"] = api_key
+
+        uploaded_file = st.file_uploader(
+            "Upload a file: ", type=["pdf", "docx", "txt"])
+        chunk_size = st.slider(
+            label="Chunk Size", min_value=100, max_value=2048, value=512)
+        k = st.slider(label="K Value", min_value=1, max_value=20, value=3)
+        add_data = st.button("Add Data")
+        
+        if uploaded_file and add_data:
+            with st.spinner("Reading...Chunking... and adding embedding file....."):
+            # writing the file from RAM to the current directory on disk
+                bytes_data = uploaded_file.read()
+                file_name = os.path.join('./', uploaded_file.name)
+                with open(file_name, 'wb') as f:
+                    f.write(bytes_data)
+
+                data = load_document(file_name)
+                chunks = chunk_data(data, chunk_size=chunk_size)
+                st.write(f'Chunk size: {chunk_size}, Chunks: {len(chunks)}')
+
+                tokens, embedding_cost = calculate_embedding_cost(chunks)
+                st.write(f'Embedding cost: ${embedding_cost:.4f}')
+
+                # creating the embeddings and returning the Chroma vector store
+                vector_store = create_embeddings(chunks)
+
+                # saving the vector store in the streamlit session state (to be persistent between reruns)
+                st.session_state.vs = vector_store
+                st.success('File uploaded, chunked and embedded successfully.')
+    # user's question text input widget
+    q = st.text_input('Ask a question about the content of your file:')
+    if q: # if the user entered a question and hit enter
+        standard_answer = "Answer only based on the text you received as input. Don't search external sources. " \
+                          "If you can't answer then return `I DONT KNOW`."
+        q = f"{q} {standard_answer}"
+        if 'vs' in st.session_state: # if there's the vector store (user uploaded, split and embedded a file)
+            vector_store = st.session_state.vs
+            st.write(f'k: {k}')
+            answer = ask_and_get_answer(vector_store, q, k)
+
+            # text area widget for the LLM answer
+            st.text_area('LLM Answer: ', value=answer)
+
+            st.divider()
+
+            # if there's no chat history in the session state, create it
+            if 'history' not in st.session_state:
+                st.session_state.history = ''
+
+            # the current question and answer
+            value = f'Q: {q} \nA: {answer}'
+
+            st.session_state.history = f'{value} \n {"-" * 100} \n {st.session_state.history}'
+            h = st.session_state.history
+
+            # text area widget for the chat history
+            st.text_area(label='Chat History', value=h, key='history', height=400)
+
+# run the app: streamlit run ./chat_with_documents.py
