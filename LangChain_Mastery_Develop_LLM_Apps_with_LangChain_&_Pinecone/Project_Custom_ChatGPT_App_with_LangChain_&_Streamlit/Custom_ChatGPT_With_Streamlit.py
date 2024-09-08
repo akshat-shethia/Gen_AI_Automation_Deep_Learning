0573df792d23f77c31604faffd1da92abf001106
akshat-shethia <akshatshethia@gmail.com>
2024-09-09 01:01:50 +0530

    Starting the LangGraph Course !!!

--- LangChain_Mastery_Develop_LLM_Apps_with_LangChain_&_Pinecone/Project_Custom_ChatGPT_App_with_LangChain_&_Streamlit/Custom_ChatGPT_With_Streamlit.py
+++ LangChain_Mastery_Develop_LLM_Apps_with_LangChain_&_Pinecone/Project_Custom_ChatGPT_App_with_LangChain_&_Streamlit/Custom_ChatGPT_With_Streamlit.py
@@ -13,12 +13,12 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv(), override=True)
 
 st.set_page_config(
-    page_title='You Custom Assistant',
+    page_title='Your Custom Assistant',
     page_icon='🤖'
 )
 st.subheader('Your Custom ChatGPT 🤖')
 
-chat = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.5)
+chat = ChatOpenAI(model_name='gpt-4o-mini', temperature=0.5)
 
 # creating the messages (chat history) in the Streamlit session state
 if 'messages' not in st.session_state:
