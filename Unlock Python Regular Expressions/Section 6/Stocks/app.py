1b20be055b6ed4393930cf192f34fabb5cc3b370
akshat-shethia <akshatshethia@gmail.com>
2024-12-30 12:13:44 +0530

    Added Stocks dataset practice problem solutions

--- Unlock Python Regular Expressions/Section 6/Stocks/app.py
+++ Unlock Python Regular Expressions/Section 6/Stocks/app.py
@@ -6,3 +6,8 @@ result = re.findall(
     r"(.+?)\s+[0-9]+\.[0-9]+M\s+[1-4][0-9]\.[0-9]+B\s+[0-9]+\.[0-9]+", string)
 print(result)
 # ['3M', 'Coca-Cola', 'McDonalds', 'Merck&Co', 'Nike', 'Tesla', 'Visa A']
+
+
+# Write a pattern on line 7, in between the double quotes, that will match the company names whose revenue is between 10B and 30B and P/E ratio is between 10 and 15.
+result = re.findall(r"(.+?)\s+[0-9]+\.[0-9]+M\s+[1-2][0-9]\.[0-9]+B\s+1[0-4]\.[0-9]+", string)
+print(result)
